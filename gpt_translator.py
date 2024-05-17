93a3bc3afe5db3b80a8a0f0042292708f8b32314
未央天 <asueeer@163.com>
2024-05-17 14:25:32 +0800

    change translate

--- gpt_translator.py
+++ gpt_translator.py
@@ -1,19 +1,23 @@
+from rich import print
+
 domain = 'Advanced Network System'
 src = 'English'
 dst = 'Chinese'
 
 
 def translate(client, english_text, pred, succ):
+    prompt = (f"You are a expert in {domain}. Now you are trying to translate a lecture from {src} to"
+              f"{dst}. Your task is translate the sentence I give you from {src} into {dst}."
+              "To let you know the context of the text, I will give you the pred context and succ "
+              "context. Please only transfer the English sentence the user give you. You don't have to "
+              f"give other response. Notice the previous context is {pred}. And succ context is {succ}.")
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "system",
-                "content": f"You are a expert in {domain}. Now you are trying to translate a lecture from {src} to"
-                           f"{dst}. Your task is translate the sentence I give you from {src} into {dst}."
-                           "To let you know the context of the text, I will give you the pred context and succ "
-                           "context. Please only transfer the English sentence the user give you. You don't have to " + pred +
-                           f"give other response. Notice the previous context is {pred}. And succ context is {succ}."},
+                "content": prompt
+            },
             {
                 "role": "user",
                 "content": english_text
@@ -23,4 +27,5 @@ def translate(client, english_text, pred, succ):
         max_tokens=64,
         top_p=1
     )
+    print(response)
     return response.choices[0].message.content
